b48d299650c5c01a0a7d1763064c3993106a1ae4
Pani <1716896473@qq.com>
2024-08-27 17:10:21 +0800

    update 校验

--- src/main/java/com/pani/oj/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pani/oj/exception/GlobalExceptionHandler.java
@@ -4,8 +4,14 @@ import com.pani.oj.common.BaseResponse;
 import com.pani.oj.common.ErrorCode;
 import com.pani.oj.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 全局异常处理器
@@ -28,4 +34,18 @@ public class GlobalExceptionHandler {
         log.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseResponse<?> argumentExceptionHandler(MethodArgumentNotValidException e, HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        BindingResult bindingResult = e.getBindingResult();
+        log.error("请求[ {} ] {} 的参数校验发生错误：", request.getMethod(), request.getRequestURL());
+        for (ObjectError objectError : bindingResult.getAllErrors()) {
+            FieldError fieldError = (FieldError) objectError;
+            log.error("参数 {} = {} 校验错误：{}", fieldError.getField(), fieldError.getRejectedValue(), fieldError.getDefaultMessage());
+            sb.append("参数 ").append(fieldError.getField()).append("校验错误：").append(fieldError.getDefaultMessage());
+        }
+        return ResultUtils.error(ErrorCode.PARAMS_ERROR, String.valueOf(sb));
+    }
+
 }
